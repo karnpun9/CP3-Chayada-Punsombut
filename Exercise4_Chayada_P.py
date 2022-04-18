7118dd68615568226bbb76b10b6dd6732f9645ff
karnpun9 <103926707+karnpun9@users.noreply.github.com>
2022-04-19 01:34:02 +0700

    Update Exercise4_Chayada_P.py

--- Exercise4_Chayada_P.py
+++ Exercise4_Chayada_P.py
@@ -1,6 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>--- Your Score ---<title>
-<head>
-<body></body>
+'''Foundation English(FE)
+General Business(GB)
+Introduction to Computer Systems(ICS)
+Computer Programming(CP)'''
+FE = 82.5
+GB = 76.8
+ICS = 84
+CP = 92
+
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Busines :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
